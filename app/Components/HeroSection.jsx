a07f68382ec2de41273f3b6c20c1b7f8b4573803
Joshua Baker <joshua.aaron.baker.dev@gmail.com>
2023-09-15 20:42:09 -0700

    Demo modeling hero section site, removed all nextjs boilerplate code.

---
+++ app/Components/HeroSection.jsx
@@ -0,0 +1,13 @@
+
+export default function HeroSection() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className="col-span-7 place-self-center">
+        <section>
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Joshua</h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ut delectus perspiciatis suscipit dignissimos minima quaerat aliquid sed, fugiat officiis fugit nobis obcaecati iusto labore voluptates omnis porro saepe tempora?</p>
+        </section>
+      </div>
+    </div>
+  )
+}
