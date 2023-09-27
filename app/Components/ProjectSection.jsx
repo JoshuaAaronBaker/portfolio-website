38613362fdd211b98d244b01f4f3234a735092cb
Joshua Baker <joshua.aaron.baker.dev@gmail.com>
2023-09-26 18:47:40 -0700

    Added section semantic tag to ProjectSection component.

--- app/Components/ProjectSection.jsx
+++ app/Components/ProjectSection.jsx
@@ -59,29 +59,31 @@ const ProjectSection = () => {
 
   return (
     <>
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
-      </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag
-          onClick={handleTagChange}
-          name="All"
-          isSelected={tag === "All"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
-        />
-        {/* <ProjectTag
+    <section>
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+          My Projects
+        </h2>
+        <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+          <ProjectTag
+            onClick={handleTagChange}
+            name="All"
+            isSelected={tag === "All"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="Web"
+            isSelected={tag === "Web"}
+          />
+          {/* <ProjectTag
           onClick={handleTagChange}
           name="Mobile"
           isSelected={tag === "Mobile"}
         /> */}
-      </div>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">{filteredProjects.map((project) =>
-    <ProjectCard key={project.id} title={project.title} technologies={project.technologies} description={project.description} imgUrl={project.imgUrl} gitUrl={project.gitUrl} previewUrl={project.previewUrl}/>)}
-    </div>
+        </div>
+        <div className="grid md:grid-cols-3 gap-8 md:gap-12">{filteredProjects.map((project) =>
+          <ProjectCard key={project.id} title={project.title} technologies={project.technologies} description={project.description} imgUrl={project.imgUrl} gitUrl={project.gitUrl} previewUrl={project.previewUrl} />)}
+        </div>
+    </section>
     </>
   )
 }
